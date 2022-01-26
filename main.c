8b2bba7ebf309876515e93ed2c8468d4aa763c7c
MD Alimuzzaman Haris <73691596+Az-Haris@users.noreply.github.com>
2022-01-27 01:42:40 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+
+    a=1000;
+
+    while(a>=1)
+    {
+        printf("%d Az Haris\n");
+        a--;
+    }
+
+
+    return 0;
+}
